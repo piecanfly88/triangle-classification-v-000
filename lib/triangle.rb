28be06ab8476a1da33927e31c0fb500491d802ae
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-10 20:30:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(triangle)
+    triangle.each{|key, value| self.send("#{key}=", value)}
+    if s1 + s2 < s3 || s1 + s3 < s2 || s2 + s3 < s1 || s1 + s2 + s3 == 0
+      raise TriangleError
+    end 
+  end
+
+  def kind
+
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
